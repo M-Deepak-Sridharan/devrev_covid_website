473c8c180b0560724115dcd4a2b4b32802060c64
balajiofficial06 <106816231+balajiofficial06@users.noreply.github.com>
2022-12-18 22:19:21 +0530

    Update bookVaccine.js

--- DEVREV/covid-website/backend/controller/bookVaccine.js
+++ DEVREV/covid-website/backend/controller/bookVaccine.js
@@ -1,10 +1,10 @@
+const { json } = require("express");
 const userModel = require("../models/user");
 const vaccinationModel = require("../models/vaccination_centre");
 
 exports.bookVaccination = async (req, res, next) => {
-  const totalLot = await vaccinationModel
-    .find({ name: req.body.centre })
-    .select("totalDose");
-  console.log(req.body.centre);
-  res.send(totalLot.totalDose);
+  const location = await vaccinationModel.find({ pincode: req.body.pincode });
+  const dose = location[0].totalDose;
+  console.log(dose);
+  res.send(location);
 };
