9c2b66f9f0ea318fadd4c6dd09b20fb9b3bf9bac
balajiofficial06 <106816231+balajiofficial06@users.noreply.github.com>
2022-12-18 10:41:26 +0530

    Update server.js

--- DEVREV/covid-website/backend/server.js
+++ DEVREV/covid-website/backend/server.js
@@ -1,30 +1,27 @@
-const express=require('express');
-const cors=require('cors');
+const express = require("express");
+const cors = require("cors");
 
 // require('dotenv').config();
 
-const app=express();
-
+const app = express();
 
 // app.use(cors());
 app.use(express.json());
 
-app.listen(5000, ()=>{
-    console.log('Server is running on port :${port}');
+app.listen(5000, () => {
+  console.log("Server is running on port :${port}");
 });
 
-app.post("/post",async(req,res)=>{
-    console.log(req.body);
-    const {data}=req.body;
-     try{
-    if(req.data=="deepak"){
-        
-        res.send({status:"ok"})}
-        else{
-            res.send({status:"Sser not found"});
-        }
-
-    }catch(error){
-        res.send({status:"error"})
+app.post("/post", async (req, res) => {
+  console.log(req.body);
+  const { data } = req.body;
+  try {
+    if (req.data == "deepak") {
+      res.send({ status: "ok" });
+    } else {
+      res.send({ status: "Sser not found" });
     }
+  } catch (error) {
+    res.send({ status: "error" });
+  }
 });
