f6eec8aa441471e71687dd5f88cd230bdc1cdce0
M-Deepak-Sridharan <78675261+M-Deepak-Sridharan@users.noreply.github.com>
2022-12-18 10:22:16 +0530

    updated

---
+++ DEVREV/covid-website/backend/server.js
@@ -0,0 +1,30 @@
+const express=require('express');
+const cors=require('cors');
+
+// require('dotenv').config();
+
+const app=express();
+
+
+// app.use(cors());
+app.use(express.json());
+
+app.listen(5000, ()=>{
+    console.log('Server is running on port :${port}');
+});
+
+app.post("/post",async(req,res)=>{
+    console.log(req.body);
+    const {data}=req.body;
+     try{
+    if(req.data=="deepak"){
+        
+        res.send({status:"ok"})}
+        else{
+            res.send({status:"Sser not found"});
+        }
+
+    }catch(error){
+        res.send({status:"error"})
+    }
+});
