c6eff0222556f2aba05157b35dd227864092e53a
M-Deepak-Sridharan <78675261+M-Deepak-Sridharan@users.noreply.github.com>
2022-12-18 15:59:39 +0530

    frontend removes

--- DEVREV/covid-website/backend/server.js
+++ DEVREV/covid-website/backend/server.js
@@ -9,24 +9,11 @@ const app = express();
 // app.use(cors());
 app.use(express.json());
 
-<<<<<<< HEAD
-const mongoUrl =
-  "mongodb+srv://deepak:deepak1234@cluster0.hx0fmph.mongodb.net/?retryWrites=true&w=majority";
-mongoose
-  .connect(mongoUrl, {
-    useNewUrlParser: true,
-  })
-  .then(() => {
-    console.log("Connected to database");
-  })
-  .catch((e) => console.log(e));
-=======
 const mongoUrl="mongodb+srv://deepak:deepak1234@cluster0.hx0fmph.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(mongoUrl,{
     useNewUrlParser:true
 }).then(()=>{console.log("Connected to database");})
 .catch(e=>console.log(e));
->>>>>>> 4f5e45e5f4438a0b280c859f68bda4ba98417687
 
 app.listen(5000, () => {
   console.log("Server is running on port :${port}");
