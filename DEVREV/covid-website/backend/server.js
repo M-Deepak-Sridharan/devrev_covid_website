a96955e3a3fd6ef8c120d8eec40d959dfaf2b0e8
balajiofficial06 <106816231+balajiofficial06@users.noreply.github.com>
2022-12-18 13:00:26 +0530

    Update server.js

--- DEVREV/covid-website/backend/server.js
+++ DEVREV/covid-website/backend/server.js
@@ -9,7 +9,6 @@ const app = express();
 // app.use(cors());
 app.use(express.json());
 
-<<<<<<< HEAD
 const mongoUrl =
   "mongodb+srv://deepak:deepak1234@cluster0.hx0fmph.mongodb.net/?retryWrites=true&w=majority";
 mongoose
@@ -20,13 +19,6 @@ mongoose
     console.log("Connected to database");
   })
   .catch((e) => console.log(e));
-=======
-const mongoUrl="mongodb+srv://deepak:deepak1234@cluster0.hx0fmph.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect(mongoUrl,{
-    useNewUrlParser:true
-}).then(()=>{console.log("Connected to database");})
-.catch(e=>console.log(e));
->>>>>>> 4f5e45e5f4438a0b280c859f68bda4ba98417687
 
 app.listen(5000, () => {
   console.log("Server is running on port :${port}");
